3c52302dbe0bf1a421901df99a91da89df4151bb
suntech123 <skumarbehl@gmail.com>
2017-12-05 12:58:20 +0530

    Create suntest-parser-VA_HoldingDetail-full.py

---
+++ Python/suntest-parser-VA_HoldingDetail-full.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################################################
+#
+#
+#
+#
+##############################################################################################################
+import os
+import sys
+import csv
+import shutil
+import string
+import time
+from xml.etree.cElementTree import iterparse
+import datetime
+#from datetime import timedelta
+import re
+#import logging
+#doc = iterparse('/data6/splice/July_Files/DataWarehouse37/DataWarehouse37_FO_USA_M_201608034.xml', ('start', 'end'))
+doc = iterparse('/home/cloudera/Desktop/sample-raw.xml', ('start', 'end'))
+path='CUSIP/CUSIP'
+path_parts = path.split('/')
+#next(doc)
+#tag_stack = []
+#elem_stack = []
+st=datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H_%M_%S')
+#print(st)
+rec=[]
+c_rec=[]
+HoldingDetail_Ordered_key=['HoldingDetailId','DetailHoldingTypeId','CUSIP','ISIN','Ticker','SecurityName','Weighting','NumberOfShare','MarketValue']
+HoldingDetail={'HoldingDetailId':'','DetailHoldingTypeId':'','CUSIP':'','ISIN':'','Ticker':'','SecurityName':'','Weighting':'','NumberOfShare':'','MarketValue':''}
+for event, elem in doc:
+#     nm=elem.find('PackageName')
+#     nm=elem.find('InvestmentVehicle')
+#     if nm is not None:
+#         print(nm.get('_Id'))
+#        print(nm.text)
+    if event == 'start':
+#        nm=elem.find('InvestmentVehicle')
+         if elem.tag == 'InvestmentVehicle':
+#            print(elem.attrib['_Id'])
+            rec.append(elem.attrib['_Id'])
+         if elem.tag == 'HoldingDetailId' or elem.tag == 'DetailHoldingTypeId' or elem.tag == 'SecurityName' or elem.tag == 'NumberOfShare' or elem.tag == 'MarketValue':
+#         or elem.tag == 'MarketValue':
+            if elem.text is None:
+               str_val=''
+            else:
+               str_val=elem.text
+#            str_val_lst.append(str_val)
+            HoldingDetail[elem.tag]=str_val 
+#            str_val_rec='|'.join(str_val_lst)
+#            c_rec.append(str_val_rec)
+    if event == 'end':
+         if elem.tag == 'HoldingDetail':
+            str_val_rec=u'|'.join(HoldingDetail[x] for x in HoldingDetail_Ordered_key).encode('utf-8').strip()
+            c_rec.append(str_val_rec)
+            HoldingDetail=dict.fromkeys(HoldingDetail,'')
+         if elem.tag == 'InvestmentVehicle':
+            rec_dump='|'.join(rec)
+            for i in range(0,len(c_rec)):
+                print(rec_dump + '|' + str(c_rec[i]))
+            del rec[:]
+            del c_rec[:]
+                        
+    elem.clear()
+#       tag_stack.append(elem.tag)
+#       elem_stack.append(elem)
+#    elif event == 'end':
+#         if tag_stack == path_parts:
+#            print(elem)
+#            elem_stack[-2].remove(elem)
+#         try:
+#            tag_stack.pop()
+#            elem_stack.pop()
+#         except IndexError:
+#            pass
+ed=datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H_%M_%S')
+print(st)
+print(ed)
+sys.exit(0)
