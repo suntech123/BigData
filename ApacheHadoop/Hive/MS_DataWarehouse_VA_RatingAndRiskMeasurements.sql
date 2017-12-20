4c25c5aa569ee38de909d77688066a9dd4035000
suntech123 <skumarbehl@gmail.com>
2017-12-20 14:54:38 +0530

    Create MS_DataWarehouse_VA_RatingAndRiskMeasurements.sql

---
+++ ApacheHadoop/Hive/MS_DataWarehouse_VA_RatingAndRiskMeasurements.sql
@@ -0,0 +1,3 @@
+add jar /home/kumarsu/hivexmlserde-1.0.5.3.jar;
+insert overwrite directory '/data/MORNINGSTAR/CSV' row format delimited fields terminated by '\u0001' NULL DEFINED AS ''
+select * from MorningStar.MS_DataWarehouse_VA_RatingAndRiskMeasurements;
