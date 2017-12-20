f3459ed370e46cff99c380a03ed3d01d36a03cbe
suntech123 <skumarbehl@gmail.com>
2017-12-20 14:39:37 +0530

    Create MS_DataWarehouse_NonVA_HoldingDetail.sql

---
+++ ApacheHadoop/Hive/MS_DataWarehouse_NonVA_HoldingDetail.sql
@@ -0,0 +1,4 @@
+add jar /home/kumarsu/hivexmlserde-1.0.5.3.jar;
+insert overwrite directory '/data/MORNINGSTAR/CSV' row format delimited fields terminated by '\u0001' NULL DEFINED AS '' 
+SELECT InvestmentVehicle_Id, CUSIP,ISIN,exp_HoldingDetail.HoldingDetail["Ticker"],exp_HoldingDetail.HoldingDetail["HoldingDetailId"],exp_HoldingDetail.HoldingDetail["DetailHoldingTypeId"],exp_HoldingDetail.HoldingDetail["SecurityName"],exp_HoldingDetail.HoldingDetail["Weighting"],exp_HoldingDetail.HoldingDetail["NumberOfShare"],exp_HoldingDetail.HoldingDetail["MarketValue"]
+FROM MorningStar.MS_DataWarehouse_NonVA_HoldingDetail LATERAL VIEW explode(HoldingDetails) v1 AS exp_HoldingDetail;
