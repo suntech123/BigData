6ace303ea9948857fdd428901c7034bea9b0f138
suntech123 <skumarbehl@gmail.com>
2018-05-16 11:57:33 +0530

    Update simple_topic_based_consumer.py

--- ApacheSpark/Python-Spark/Kafka-Python-Client/simple_topic_based_consumer.py
+++ ApacheSpark/Python-Spark/Kafka-Python-Client/simple_topic_based_consumer.py
@@ -1,4 +1,8 @@
-
+#!/usr/bin/python
+"""
+ Consumes messages from a kafka topic
+ Usage: ./simple_topic_based_consumer.py
+"""
 from kafka import KafkaConsumer
 consumer = KafkaConsumer('test')
 for msg in consumer:
